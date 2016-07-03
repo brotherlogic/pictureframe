a86c0b8ba041686ce33f51bcf7863d0d9bbc83ea
Simon Tucker <brotherlogic@gmail.com>
2016-07-03 10:12:12 -0700

    Added framework to support auto load. This closes #1.

--- src/main/java/com/github/brotherlogic/pictureframe/Frame.java
+++ src/main/java/com/github/brotherlogic/pictureframe/Frame.java
@@ -1,9 +1,11 @@
 package com.github.brotherlogic.pictureframe;
 
 public class Frame extends FrameBase {
-
 	public static void main(String[] args) {
 		Frame f = new Frame();
-		System.out.println(f.getGreeting());
+		FrameDisplay d = new FrameDisplay();
+		d.setSize(200, 200);
+		d.setLocationRelativeTo(null);
+		d.setVisible(true);
 	}
 }
