0a67485842b204b156d4f70d4566fb5d28ba0faf
Simon Tucker <brotherlogic@gmail.com>
2016-07-03 16:46:36 -0700

    Quick fix for screen layout

--- src/main/java/com/github/brotherlogic/pictureframe/Frame.java
+++ src/main/java/com/github/brotherlogic/pictureframe/Frame.java
@@ -6,8 +6,8 @@ public class Frame extends FrameBase {
 	public static void main(String[] args) {
 		Frame f = new Frame();
 		FrameDisplay d = new FrameDisplay();
+                d.setSize(800, 480);
 		d.setLocationRelativeTo(null);
-		d.setSize(800, 480);
 		d.setVisible(true);
 	}
 }
