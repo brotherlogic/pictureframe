af0955fd0de3bce36b7365a66369011e2071dee1
brotherlogic <brotherlogic@gmail.com>
2016-07-03 07:29:02 -0700

    Created a basic infrastructure. This closes #3.

---
+++ src/main/java/com/github/brotherlogic/pictureframe/Frame.java
@@ -0,0 +1,9 @@
+package com.github.brotherlogic.pictureframe;
+
+public class Frame extends FrameBase {
+
+	public static void main(String[] args) {
+		Frame f = new Frame();
+		System.out.println(f.getGreeting());
+	}
+}
