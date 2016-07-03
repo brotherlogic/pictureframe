dd268d5a2cf14ca084fff4fa84b329b61bf90764
Simon Tucker <brotherlogic@gmail.com>
2016-07-03 16:27:20 -0700

    Forces window to be full screen. This closes #5.

--- src/main/java/com/github/brotherlogic/pictureframe/Frame.java
+++ src/main/java/com/github/brotherlogic/pictureframe/Frame.java
@@ -1,11 +1,13 @@
 package com.github.brotherlogic.pictureframe;
 
+import javax.swing.JFrame;
+
 public class Frame extends FrameBase {
 	public static void main(String[] args) {
 		Frame f = new Frame();
 		FrameDisplay d = new FrameDisplay();
-		d.setSize(200, 200);
 		d.setLocationRelativeTo(null);
+		d.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		d.setVisible(true);
 	}
 }
