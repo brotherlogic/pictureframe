dc30ca227d805294ff0d1b158fe197b364f3bf23
Simon Tucker <brotherlogic@gmail.com>
2016-07-30 20:56:27 -0700

    Updated syncer

--- syncer.sh
+++ syncer.sh
@@ -1,5 +1,5 @@
 git fetch -p
 git merge origin/master
 rm pictureframe.jar
-rsync -avz --progress $1:codestore/pictureframe/ ./codestore
+rsync -e 'ssh -p 2223' -avz --progress $1:codestore/pictureframe/ ./codestore
 ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
