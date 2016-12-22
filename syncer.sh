6f6e0b4d76391784083006e3b596d578a3563543
Simon Tucker <brotherlogic@gmail.com>
2016-12-22 08:17:25 -0800

    Added gc to syncer

--- syncer.sh
+++ syncer.sh
@@ -1,5 +1,6 @@
 git fetch -p
 git merge origin/master
+git gc
 rm pictureframe.jar
 rsync -e 'ssh -p 2223' -avz --progress $1:codestore/pictureframe/ ./codestore
 ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
