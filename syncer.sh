ad525d93c6614b1d8987e5152395a9e56198b818
brotherlogic <brotherlogic@gmail.com>
2017-04-08 20:26:20 -0700

    Bump

--- syncer.sh
+++ syncer.sh
@@ -1,7 +1,7 @@
+ls
 git fetch -p
 git merge origin/master
 git gc
 rm pictureframe.jar
 rsync -e 'ssh -p 2223' -avz --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
 ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
-ls
