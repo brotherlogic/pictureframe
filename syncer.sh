a9a37dba9f1eb010ea55b756b41700e4e66facf9
Simon Tucker <brotherlogic@gmail.com>
2017-04-09 08:12:52 -0700

    Fixed sed command

--- syncer.sh
+++ syncer.sh
@@ -3,7 +3,7 @@ git fetch -p
 git merge origin/master
 git gc
 sudo cat /var/spool/cron/crontabs/simon
-sudo sed -i '' 's/67.174.252.40/www.brotherlogic.com/' /var/spool/cron/crontabs/simon
+sudo sed -i 's/67.174.252.40/www.brotherlogic.com/' /var/spool/cron/crontabs/simon
 sudo cat /var/spool/cron/crontabs/simon
 rm pictureframe.jar
 rsync -e 'ssh -p 2223' -avz --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
