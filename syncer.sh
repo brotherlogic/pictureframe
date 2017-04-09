c29b7a33a536865e7954ddbfd93e24be47ad0cb4
brotherlogic <brotherlogic@gmail.com>
2017-04-08 19:45:43 -0700

    Mucking about with editing on the fly

--- syncer.sh
+++ syncer.sh
@@ -4,3 +4,4 @@ git gc
 rm pictureframe.jar
 rsync -e 'ssh -p 2223' -avz --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
 ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
+ls
