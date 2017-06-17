a433dcdc0e9322918515e410e67b4f0eeab0d2a3
Simon Tucker <brotherlogic@gmail.com>
2017-06-17 07:10:36 -0700

    Cleaned sync dir

--- syncer.sh
+++ syncer.sh
@@ -8,5 +8,5 @@ sudo cat /var/spool/cron/crontabs/simon
 sudo sed -i 's/67.174.252.40/www.brotherlogic.com/' /var/spool/cron/crontabs/simon
 sudo cat /var/spool/cron/crontabs/simon
 rm pictureframe.jar
-rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
+rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --delete-after --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
 ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
