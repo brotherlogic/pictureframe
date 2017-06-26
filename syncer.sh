4a71f906f1ed3d70aaa23cb8e98f7f691e037491
Simon Tucker <brotherlogic@gmail.com>
2017-06-25 19:49:37 -0700

    Tidying up dads frome. This closes #58.

--- syncer.sh
+++ syncer.sh
@@ -1,3 +1,6 @@
+cat /home/simon/.xsession
+sed -i 's/--host 67.174.252.40 --port 50055/--server www.brotherlogic.com/' /home/simon/.xsession
+cat /home/simon/.xsession
 git fetch -p
 cat ~/.xsession
 git merge origin/master
@@ -5,4 +8,3 @@ git gc
 rm pictureframe.jar
 rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --delete-after --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
 ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
-ip route
