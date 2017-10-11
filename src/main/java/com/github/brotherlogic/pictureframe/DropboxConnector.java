01d8629b449ab86f08273220357cf0796a7ef82b
Simon Tucker <brotherlogic@gmail.com>
2017-10-10 20:53:59 -0700

    Fixed for new dropbox

--- src/main/java/com/github/brotherlogic/pictureframe/DropboxConnector.java
+++ src/main/java/com/github/brotherlogic/pictureframe/DropboxConnector.java
@@ -7,50 +7,39 @@ import java.io.InputStreamReader;
 import java.util.Locale;
 
 import com.dropbox.core.DbxAppInfo;
-import com.dropbox.core.DbxAuthFinish;
-import com.dropbox.core.DbxClient;
-import com.dropbox.core.DbxEntry;
 import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.DbxWebAuthNoRedirect;
+import com.dropbox.core.DbxWebAuth;
+import com.dropbox.core.v1.DbxEntry;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+
 
 public class DropboxConnector {
 
-	DbxClient client;
+	DbxClientV2 client;
 
 	DbxRequestConfig config;
 
 	public DropboxConnector(String token) {
-		config = new DbxRequestConfig("TuckerPictureFrame/1.0", Locale.getDefault().toString());
-		client = new DbxClient(config, token);
+		config = DbxRequestConfig.newBuilder("TuckerPictureFrame/1.0").build();
+		client = new DbxClientV2(config, token);
 	}
 
-	public void login(String APP_KEY, String APP_SECRET) throws Exception {
-		DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
-		DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
-
-		// Have the user sign in and authorize your app.
-		String authorizeUrl = webAuth.start();
-		System.out.println("1. Go to: " + authorizeUrl);
-		System.out.println("2. Click \"Allow\" (you might have to log in first)");
-		System.out.println("3. Copy the authorization code.");
-		String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
-
-		// This will fail if the user enters an invalid authorization code.
-		DbxAuthFinish authFinish = webAuth.finish(code);
-		String accessToken = authFinish.accessToken;
-		System.out.println("TOKEN = " + accessToken);
-	}
 
 	public void syncFolder(String inputDirectory, File outputDirectory) throws Exception {
-		DbxEntry.WithChildren listing = client.getMetadataWithChildren(inputDirectory);
-		for (DbxEntry child : listing.children) {
-			File f = new File(outputDirectory.getAbsolutePath() + "/" + child.name);
-			System.out.println("Syncing with " + child.name + " -> " + f.exists());
+		System.out.println("SEARCHING: " + inputDirectory);
+		ListFolderResult result = client.files().listFolder(inputDirectory);
+		for (Metadata child : result.getEntries()) {
+			File f = new File(outputDirectory.getAbsolutePath() + "/" + child.getPathDisplay());
+			System.out.println("Syncing with " + child.getPathDisplay() + " -> " + f.exists());
 			if (!f.exists()) {
 				FileOutputStream outputStream = new FileOutputStream(
-						outputDirectory.getAbsolutePath() + "/" + child.name);
+						outputDirectory.getAbsolutePath() + "/" + child.getPathDisplay());
 				try {
-					client.getFile(child.path, null, outputStream);
+					System.out.println("FROM: " + child.getPathDisplay());
+					System.out.println("TO: " + outputDirectory.getAbsolutePath() + "/" + child.getPathDisplay());
+					client.files().download(child.getPathDisplay()).download(outputStream);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}finally {
@@ -62,6 +51,6 @@ public class DropboxConnector {
 
 	public static void main(String[] args) throws Exception {
 		DropboxConnector connector = new DropboxConnector(args[0]);
-		connector.syncFolder("/", new File("testout"));
+		connector.syncFolder("", new File("testout"));
 	}
 }
