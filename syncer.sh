d50786bd15be191edddbb165a085f6f89b41ac5c
Simon Tucker <brotherlogic@gmail.com>
2017-10-15 11:56:30 -0700

    Turned off printing

--- syncer.sh
+++ syncer.sh
@@ -4,7 +4,7 @@ if ping -c 1 www.github.com > /dev/null 2>&1 ; then
     git gc
     rm pictureframe.jar
     cat ~/.ssh/id_rsa.pub
-    ssh-keygen -f "/home/simon/.ssh/known_hosts" -R [$(curl -s "$1"/resolve | awk -F ':' '{print $1}')]:2223
+    #ssh-keygen -f "/home/simon/.ssh/known_hosts" -R [$(curl -s "$1"/resolve | awk -F ':' '{print $1}')]:2223
     rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --delete-after --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
     ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
 fi
