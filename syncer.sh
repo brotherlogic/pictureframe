3ed642eada286c84c049b1c975b383017fc92cc4
Simon Tucker <brotherlogic@gmail.com>
2018-01-29 10:23:54 -0800

    Reverting

--- syncer.sh
+++ syncer.sh
@@ -3,7 +3,7 @@ if ping -c 1 www.github.com > /dev/null 2>&1 ; then
     git merge origin/master
     git gc
     rm pictureframe.jar
-    df -h
+    #df -h
     #cat ~/.ssh/id_rsa.pub
     #ssh-keygen -f "/home/simon/.ssh/known_hosts" -R [$(curl -s "$1"/resolve | awk -F ':' '{print $1}')]:2223
     rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --delete-after --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
