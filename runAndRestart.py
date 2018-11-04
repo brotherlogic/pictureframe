e292a6a9a0b37aa8337d57168c851abc6f0458ab
Simon Tucker <simon@brotherlogic@gmail>
2018-11-03 17:51:20 -0700

    Added a verbose mode

--- runAndRestart.py
+++ runAndRestart.py
@@ -1,9 +1,16 @@
 import os
 import sys
 
+verbose = True
+
+if verbose:
+    print "Running in verbose mode"
+
 lines_before = len(os.popen('find ./codestore').readlines())
-print "\n".join(os.popen('./syncer.sh ' + sys.argv[1]).readlines())
-#os.popen('./syncer.sh ' + sys.argv[1]).readlines()
+if verbose:
+    print "\n".join(os.popen('./syncer.sh ' + sys.argv[1]).readlines())
+else:
+    os.popen('./syncer.sh ' + sys.argv[1]).readlines()
 lines_after = len(os.popen('find ./codestore').readlines())
 
 if lines_before != lines_after:
