6a1bc380bcfe5694a7b1cde136f2bad7c5c384d3
Simon Tucker <brotherlogic@gmail.com>
2018-11-03 17:22:26 -0700

    Print out syncer response

--- runAndRestart.py
+++ runAndRestart.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 lines_before = len(os.popen('find ./codestore').readlines())
-#print "\n".join(os.popen('./syncer.sh ' + sys.argv[1]).readlines())
-os.popen('./syncer.sh ' + sys.argv[1]).readlines()
+print "\n".join(os.popen('./syncer.sh ' + sys.argv[1]).readlines())
+#os.popen('./syncer.sh ' + sys.argv[1]).readlines()
 lines_after = len(os.popen('find ./codestore').readlines())
 
 if lines_before != lines_after:
