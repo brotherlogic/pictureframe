07296fda6b8e8741146b33614176192f6c8e87ee
Simon Tucker <simon@brotherlogic@gmail>
2019-01-26 17:58:50 -0800

    Removes removal

--- syncer.sh
+++ syncer.sh
@@ -8,7 +8,7 @@ if ping -c 1 www.github.com > /dev/null 2>&1 ; then
     rm pictureframe.jar
     git config --global user.email "brotherlogic@gmail.com"
     git config --global user.name "Simon Tucker"
-    ssh-keygen -f "/home/simon/.ssh/known_hosts" -R [73.162.90.182]:2223
+    #ssh-keygen -f "/home/simon/.ssh/known_hosts" -R [73.162.90.182]:2223
     rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --delete-after --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
     ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
 fi
