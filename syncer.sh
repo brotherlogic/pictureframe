6ff093588186f92a7c85b5d912aa3305be0a7e1e
Simon Tucker <simon@brotherlogic@gmail>
2019-01-26 17:48:40 -0800

    Fixes for frame two

--- syncer.sh
+++ syncer.sh
@@ -1,4 +1,3 @@
-rm  .git/objects/pack/pack-84bd4c3921ee8efb7151c7178159056102439998.idx
 cat ~/xs.txt
 df -h
 ls -ltr ~
@@ -9,7 +8,7 @@ if ping -c 1 www.github.com > /dev/null 2>&1 ; then
     rm pictureframe.jar
     git config --global user.email "brotherlogic@gmail.com"
     git config --global user.name "Simon Tucker"
-    #ssh-keygen -f "/home/simon/.ssh/known_hosts" -R [73.162.90.182]:2223
+    ssh-keygen -f "/home/simon/.ssh/known_hosts" -R [73.162.90.182]:2223
     rsync -e 'ssh -p 2223 -o StrictHostKeyChecking=no' -avz --delete-after --progress $(curl -s "$1"/resolve | awk -F ':' '{print $1}'):codestore/pictureframe/ ./codestore
     ln -s $(find codestore/ -printf "%T+\t%p\n" | sort | grep with-dependencies | awk '{ print $2 }' | grep .jar$ | tail -n 1) ./pictureframe.jar
 fi
